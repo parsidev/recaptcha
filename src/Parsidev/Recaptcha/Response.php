8fb27233458dcaab3d3e1f4fb6874551137ea521
Mohammad Reza <parsidev@users.noreply.github.com>
2017-03-01 17:15:12 +0330

    Create Response.php

---
+++ src/Parsidev/Recaptcha/Response.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Parsidev\Recaptcha;
+
+
+class Response
+{
+    private $success = false;
+    private $errorCodes = array();
+
+    public static function fromJson($json)
+    {
+        $responseData = json_decode($json, true);
+        if (!$responseData) {
+            return new Response(false, array('invalid-json'));
+        }
+        if (isset($responseData['success']) && $responseData['success'] == true) {
+            return new Response(true);
+        }
+        if (isset($responseData['error-codes']) && is_array($responseData['error-codes'])) {
+            return new Response(false, $responseData['error-codes']);
+        }
+        return new Response(false);
+    }
+
+    public function __construct($success, array $errorCodes = array())
+    {
+        $this->success = $success;
+        $this->errorCodes = $errorCodes;
+    }
+
+    public function isSuccess()
+    {
+        return $this->success;
+    }
+
+    public function getErrorCodes()
+    {
+        return $this->errorCodes;
+    }
+
+}
